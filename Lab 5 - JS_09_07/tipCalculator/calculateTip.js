406449b71eed989513c5a8d2348b6b8e8621d860
Aaron Gezai <aaaron769@gmail.com>
2019-07-01 16:17:31 -0500

    bugyy and javascript and tip Calculator

---
+++ Lab 5 - JS_09_07/tipCalculator/calculateTip.js
@@ -0,0 +1,11 @@
+// Aaon Gezai tip calculator js file
+function calculateTotal() {
+        var subtotalElem = document.getElementById('subtotal');
+        var tipElem = document.getElementById('tip');
+        var totalElem = document.getElementById('total');
+        var subtotal = parseInt(subtotalElem.value);
+        var tip = parseInt(tipElem.value);
+        var total =( subtotal ) + ((tip*subtotal)/100);
+        totalElem.innerHTML = '$' + total;
+
+}
